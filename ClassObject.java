130fbab762cbbc8c0be3be2929a726033a28c705
Nehasingh1300 <40912375+Nehasingh1300@users.noreply.github.com>
2020-01-20 17:17:44 +0530

    Create ClassObject.java

---
+++ ClassObject.java
@@ -0,0 +1,36 @@
+public class TestCircle {  
+
+   public static void main(String[] args) {
+   
+      Circle c1 = new Circle();
+      System.out.println("The circle has radius of " 
+         + c1.getRadius() + " and area of " + c1.getArea());
+  
+      Circle c2 = new Circle(2.0);
+      System.out.println("The circle has radius of " 
+         + c2.getRadius() + " and area of " + c2.getArea());
+   }
+}
+
+class Circle {  
+   private double radius;
+   private String color;
+   
+   public Circle() {
+      radius = 1.0;
+      color = "red";
+   }
+  
+  public Circle(double r) {
+      radius = r;
+      color = "red";
+   }
+   
+   public double getRadius() {
+     return radius; 
+   }
+   
+   public double getArea() {
+      return radius*radius*Math.PI;
+   }
+}
