0090d02c09824920e57b72f4a4ce0ff8f27e61c1
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-27 13:52:45 +0530

    Package file pkg2

---
+++ Package2.java
@@ -0,0 +1,26 @@
+package pkg2;
+
+public class methods {
+
+	int sum(int a, int b) {
+		return a+b;		
+	}
+	
+	int sub(int a, int b) {
+		return a-b;		
+	}
+	
+	int mul(int a, int b) {
+		return a*b;		
+	}
+	
+	int div(int a, int b) {
+		return a/b;		
+	}
+	
+	int mod(int a, int b) {
+		return a%b;		
+	}
+	
+		
+}
