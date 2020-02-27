3db794e0e4be8c1dd1610edd411a7a9379f1ec64
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-27 13:53:36 +0530

    27-02-20

---
+++ balancepkg.java
@@ -0,0 +1,12 @@
+package Balance;
+import java.util.Scanner;
+
+public class Account {
+
+		public void DisplayMethod(){
+			Scanner sc = new Scanner(System.in);
+			System.out.println("Enter Your Balance:");
+			int balance = sc.nextInt();
+		}
+}
+
