88181745ff5304e8f4a6d7855479113d84a3e696
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-02-27 13:55:16 +0530

    access balance

---
+++ accessBALANCE.java
@@ -0,0 +1,11 @@
+package Balance;
+
+public class accessAccount {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Account obj = new Account();
+		obj.DisplayMethod();
+	}
+
+}
