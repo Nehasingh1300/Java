8e37c2e4ac1a24b9832fea67072872ec097d6688
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 17:46:55 +0530

    commit 4

---
+++ MCQ/four.java
@@ -0,0 +1,10 @@
+package random;
+
+public class four { 
+	public static void main(String args[]) { 
+		System.out.println(fun()); 
+	} 
+	static int fun() {     // earlier fun was non static and in java non static methods cannot be called without methods
+		return 20; 
+	} 
+} 
