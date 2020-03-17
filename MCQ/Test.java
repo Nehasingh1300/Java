60fe56cf8360420009513e6b1152fe1a8febdff5
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 17:41:23 +0530

    commit 2

---
+++ MCQ/Test.java
@@ -0,0 +1,13 @@
+package random;
+
+//as there is type mismatch we can just change it to true or false 
+// but we cant use int in place of boolean 
+//filename Test.java 
+class Test { 
+	public static void main(String[] args) { 
+		for(int i = 0; 1; i++) { 
+			System.out.println("Hello"); 
+			break; 
+		} 
+	} 
+} 
