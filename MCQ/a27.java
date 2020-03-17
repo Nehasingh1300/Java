6cdb9b08db5d375b377b54579ed36740241222ed
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 23:55:43 +0530

    commit 27

--- MCQ/a27.java
+++ MCQ/a27.java
@@ -1,12 +1,49 @@
-class GfG 
+// Static binding (or compile time) happens for static methods. Here p.whoAmI() calls the static method so it is called during compile time hence results in static binding and prints the method in People class.
+// p.whoAreYou() calls the method in Kid class since by default Java takes it as a virual method i.e, dynamic binding.
+
+class Person 
+{ 
+	private void who() 
+	{ 
+		System.out.println("Inside private method Person(who)"); 
+	} 
+
+	public static void whoAmI() 
+	{ 
+		System.out.println("Inside static method, Person(whoAmI)"); 
+	} 
+
+	public void whoAreYou() 
+	{ 
+		who(); 
+		System.out.println("Inside virtual method, Person(whoAreYou)"); 
+	} 
+} 
+
+class Kid extends Person 
+{ 
+	private void who() 
+	{ 
+		System.out.println("Kid(who)"); 
+	} 
+
+	public static void whoAmI() 
+	{ 
+		System.out.println("Kid(whoAmI)"); 
+	} 
+
+	public void whoAreYou() 
+	{ 
+		who(); 
+		System.out.println("Kid(whoAreYou)"); 
+	} 
+} 
+public class Gfg 
 { 
 	public static void main(String args[]) 
 	{ 
-		String s1 = new String("geeksforgeeks"); 
-		String s2 = new String("geeksforgeeks"); 
-		if (s1 == s2) 
-			System.out.println("Equal"); 
-		else
-			System.out.println("Not equal");          // s1 and s2 are two different objects the references are not the same
+		Person p = new Kid(); 
+		p.whoAmI(); 
+		p.whoAreYou(); 
 	} 
 } 
