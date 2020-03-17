22c0830b23c304c87d7d5af2a8799ef91e6ebd72
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 22:19:48 +0530

    commit 17

---
+++ MCQ/seventeen.java
@@ -0,0 +1,16 @@
+package random;
+
+public class seventeen {
+	public static void main(String args[]) 
+	{ 
+		short s = 0; 
+		int x = 07;  
+		int z = 1345;   			// Any number beginning with zero is treated as an octal number (which is 0-7).
+
+		s += z; 
+		System.out.println(  x );
+		System.out.println(  z );
+		System.out.println( s ); 
+		System.out.println(  x + z + s); 
+	} 
+} //end class 
