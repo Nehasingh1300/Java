05bf3bb430444b95979c322bfd86d1f22b34f43a
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 18:49:42 +0530

    commit 9

---
+++ MCQ/nine.java
@@ -0,0 +1,20 @@
+package random;
+
+public class nine {	 
+	int y = 2; 
+	int x = y+2; 
+	public static void main(String[] args) {	     
+		nine m = new nine(); 
+		System.out.println("x = " + m.x + ", y = " + m.y);    //  as y is intialised first  so it works perfefctly! 
+	} 
+} 
+package random;
+
+public class nine {	 
+	int y = 2; 
+	int x = y+2; 
+	public static void main(String[] args) {	     
+		nine m = new nine(); 
+		System.out.println("x = " + m.x + ", y = " + m.y);    //  as y is intialised first  so it works perfefctly! 
+	} 
+} 
