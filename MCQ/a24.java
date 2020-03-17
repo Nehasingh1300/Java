f93f84cce2731921b0ba444d6d1f3dc3f0ee3d6b
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 23:44:04 +0530

    commit 24

---
+++ MCQ/a24.java
@@ -0,0 +1,14 @@
+package random;
+
+public class a24 { 
+	public static void main(String[] args) 
+	{ 
+		StringBuilder s1 = new StringBuilder("Java"); 
+		String s2 = "Love"; 
+		s1.append(s2); 
+		s1.substring(4); 
+		int foundAt = s1.indexOf(s2); 
+		System.out.println(foundAt); 
+	} 
+} 
+// dont get it
