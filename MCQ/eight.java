4d3c1c6f08aa6eac3b13815d0572ee0a0b405904
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 18:47:43 +0530

    commit 8

---
+++ MCQ/eight.java
@@ -0,0 +1,9 @@
+package random;
+
+public class eight {      // things can really be done with the main class block also!! amazing!
+	int x = 10; 
+	public static void main(String[] args) {	 
+		eight t = new eight(); 
+		System.out.println(t.x); 
+	} 
+} 
