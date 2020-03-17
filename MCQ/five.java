70e5ca4aa235e1ba244eab8d5c8e84ab7d1533ab
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-17 18:30:51 +0530

    commit 5

---
+++ MCQ/five.java
@@ -0,0 +1,11 @@
+package random;
+
+public class five  { 
+public static void main(String args[]) { 
+	System.out.println(fun());
+} 
+static int fun() { 
+	int x= 0;   // static local variables are not allowed in java
+	return x; 
+} 
+} 
