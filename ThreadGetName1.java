13aed486b62c44d5687bd683620d17bc247620f7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-27 11:16:22 +0530

    27-3-20 {COMMIT 7}

--- ThreadGetName1.java
+++ ThreadGetName1.java
@@ -1,8 +1,8 @@
 package thread;
 
 public class GetName1  {  
- public static void main(String[] args) {  
-	 GetName1 obj = new GetName1();  
+public static void main(String[] args) {  
+    GetName1 obj = new GetName1();  
     Class Class1 = obj.getClass();  
     String nm = Class1.getName();  
     System.out.println(" The Class Name = " +nm);  
