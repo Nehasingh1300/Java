71f84383732c52f7d14f96efaa49c0f3809ac8ac
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:33:39 +0530

    Create 10.java

---
+++ Interface MCQ/10.java
@@ -0,0 +1,9 @@
+/*What type of variable can be defined in an interface?
+a) public static
+b) private final
+c) public final
+d) static final
+View Answer
+
+Answer: d
+Explanation: variable defined in an interface is implicitly final and static. They are usually written in capital letters.*/
