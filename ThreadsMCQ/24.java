f98fb0105c1c64c7b5e11fedbdba8cf1f666ea7b
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:46:20 +0530

    Create 24.java

---
+++ ThreadsMCQ/24.java
@@ -0,0 +1,9 @@
+/*Which of the following will ensure the thread will be in running state?
+a) yield()
+b) notify()
+c) wait()
+d) Thread.killThread()
+View Answer
+
+Answer: c
+Explanation: wait() always causes the current thread to go into the object’s wait pool. Hence, using this in a thread will keep it in running state.*/
