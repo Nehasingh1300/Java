932c3b297d7b62188d377f7979238a64e17794b3
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:11:28 +0530

    Create 5.java

---
+++ Collection MCQ/5.java
@@ -0,0 +1,9 @@
+/*What is Collection in Java?
+a) A group of objects
+b) A group of classes
+c) A group of interfaces
+d) None of the mentioned
+View Answer
+
+Answer: a
+Explanation: A collection is a group of objects, it is similar to String Template Library (STL) of C++ programming language.*/
