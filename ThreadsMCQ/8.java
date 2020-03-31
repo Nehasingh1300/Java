cee5c6f961d3fd5f34257ef679b51356979b3d14
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:33:12 +0530

    Create 8.java

---
+++ ThreadsMCQ/8.java
@@ -0,0 +1,9 @@
+/*What is multithreaded programming?
+a) It’s a process in which two different processes run simultaneously
+b) It’s a process in which two or more parts of same process run simultaneously
+c) It’s a process in which many different process are able to access same information
+d) It’s a process in which a single process can access information from many sources
+View Answer
+
+Answer: b
+Explanation: Multithreaded programming a process in which two or more parts of the same process run simultaneously.*/
