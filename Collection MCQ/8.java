35b609bc3ea7db721710cb3e6444a7951bd617ce
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:54:20 +0530

    Create 8.java

---
+++ Collection MCQ/8.java
@@ -0,0 +1,13 @@
+/*The collection
+
+A.extends Collections class
+
+B.extends Iterable interface
+
+C.implements Serializable interface
+
+D.implements Traversable interface
+
+Explanation
+Correct Option :B.
+Collection is an interface and it extends iterable interface. (Interface extends Interface)*/
