2eaecb487e29cd7029242fa7ffb69668c13912ce
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:48:41 +0530

    Create 1.java

---
+++ Set MCQ/1.java
@@ -0,0 +1,9 @@
+/*What is the default clone of HashSet?
+a) Deep clone
+b) Shallow clone
+c) Plain clone
+d) Hollow clone
+View Answer
+
+Answer: b
+Explanation: Default clone() method uses shallow copy. The internal elements are not cloned. A shallow copy only copies the reference object.*/
