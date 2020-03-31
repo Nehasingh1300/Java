70bb418459125cc44fa4db9947399dc360fd81df
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:42:43 +0530

    Create 17.java

---
+++ ThreadsMCQ/17.java
@@ -0,0 +1,9 @@
+/*What decides thread priority?
+a) Process
+b) Process scheduler
+c) Thread
+d) Thread scheduler
+View Answer
+
+Answer: d
+Explanation: Thread scheduler decides the priority of the thread execution. This cannot guarantee that higher priority thread will be executed first, it depends on thread scheduler implementation that is OS dependent.*/
