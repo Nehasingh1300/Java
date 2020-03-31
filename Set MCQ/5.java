59019297d09afeb6ac997ac81b3342f9735eeef3
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:50:27 +0530

    Create 5.java

---
+++ Set MCQ/5.java
@@ -0,0 +1,9 @@
+/*What is the relation between hashset and hashmap?
+a) HashSet internally implements HashMap
+b) HashMap internally implements HashSet
+c) HashMap is the interface; HashSet is the concrete class
+d) HashSet is the interface; HashMap is the concrete class
+View Answer
+
+Answer: a
+Explanation: HashSet is implemented to provide uniqueness feature which is not provided by HashMap. This also reduces code duplication and provides the memory efficient behavior of HashMap.*/
