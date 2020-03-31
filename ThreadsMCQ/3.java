ca151c54f767b6fa27d16bce9c7bb87bed504424
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:24:50 +0530

    Create 3.java

---
+++ ThreadsMCQ/3.java
@@ -0,0 +1,9 @@
+/*Which function of pre defined class Thread is used to check weather current thread being checked is still running?
+a) isAlive()
+b) Join()
+c) isRunning()
+d) Alive()
+View Answer
+
+Answer: a
+Explanation:isAlive() function is defined in class Thread, it is used for implementing multithreading and to check whether the thread called upon is still running or not.*/
