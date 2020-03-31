947d43a22ae762c63b8a9553147c7d0b84d06aef
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:22:47 +0530

    Create 3.java

---
+++ Interface MCQ/3.java
@@ -0,0 +1,9 @@
+/*Which of these access specifiers can be used for an interface?
+a) Public
+b) Protected
+c) private
+d) All of the mentioned
+View Answer
+
+Answer: a
+Explanation: Access specifier of an interface is either public or no specifier. When no access specifier is used then default access specifier is used due to which interface is available only to other members of the package in which it is declared, when declared public it can be used by any code.*/
