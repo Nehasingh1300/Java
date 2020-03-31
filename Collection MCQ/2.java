5853f02c74763745c5dd83ff8381da0b7bd89c50
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:09:41 +0530

    Create 2.java

---
+++ Collection MCQ/2.java
@@ -0,0 +1,9 @@
+/*Which of these classes is not part of Java’s collection framework?
+a) Maps
+b) Array
+c) Stack
+d) Queue
+View Answer
+
+Answer: a
+Explanation: Maps is not a part of collection framework.*/
