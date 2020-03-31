b7b63c3070c84afd41f02baaae426631c7ebf8c4
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:37:11 +0530

    Create 14.java

---
+++ Interface MCQ/14.java
@@ -0,0 +1,9 @@
+/*What happens when a constructor is defined for an interface?
+a) Compilation failure
+b) Runtime Exception
+c) The interface compiles successfully
+d) The implementing class will throw exception
+View Answer
+
+Answer: a
+Explanation: Constructor is not provided by interface as objects cannot be instantiated.*/
