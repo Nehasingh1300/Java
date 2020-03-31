980a1bef7ca064622fce5418965d9fd0eb92fea7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:40:50 +0530

    Create 1.java

---
+++ List Interface MCQ/1.java
@@ -0,0 +1,9 @@
+/*How can we remove an object from ArrayList?
+a) remove() method
+b) using Iterator
+c) remove() method and using Iterator
+d) delete() method
+View Answer
+
+Answer: c
+Explanation: There are 2 ways to remove an object from ArrayList. We can use overloaded method remove(int index) or remove(Object obj). We can also use an Iterator to remove the object.*/
