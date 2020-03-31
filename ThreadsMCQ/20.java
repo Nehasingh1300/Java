15d887c034fb696c84f7ad8b2d064e01fdc08e3f
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:44:59 +0530

    Create 20.java

---
+++ ThreadsMCQ/20.java
@@ -0,0 +1,9 @@
+/*What should not be done to avoid deadlock?
+a) Avoid using multiple threads
+b) Avoid hold several locks at once
+c) Execute foreign code while holding a lock
+d) Use interruptible locks
+View Answer
+
+Answer: c
+Explanation: To avoid deadlock situation in Java programming do not execute foreign code while holding a lock.*/
