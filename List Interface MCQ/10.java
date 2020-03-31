f9d5500a2c47eee80b275552706eeedbf502f1e7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:47:23 +0530

    Create 10.java

---
+++ List Interface MCQ/10.java
@@ -0,0 +1,7 @@
+/*When an array is passed to a method, will the content of the array undergo changes with the actions carried within the function?
+a) True
+b) False
+View Answer
+
+Answer: a
+Explanation: If we make a copy of array before any changes to the array the content will not change. Else the content of the array will undergo changes.*/
