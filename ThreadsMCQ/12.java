fc6a9230447572c935322c5230a3e22a193ac720
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:36:26 +0530

    Create 12.java

---
+++ ThreadsMCQ/12.java
@@ -0,0 +1,9 @@
+/* Which of these statements is incorrect?
+a) By multithreading CPU idle time is minimized, and we can take maximum use of it
+b) By multitasking CPU idle time is minimized, and we can take maximum use of it
+c) Two thread in Java can have the same priority
+d) A thread can exist only in two states, running and blocked
+View Answer
+
+Answer: d
+Explanation: Thread exist in several states, a thread can be running, suspended, blocked, terminated & ready to run.*/
