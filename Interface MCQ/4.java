49648cc8d1202e36c9a5e6622c0f53955c978b01
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:24:08 +0530

    Create 4.java

---
+++ Interface MCQ/4.java
@@ -0,0 +1,9 @@
+/*Which of these keywords is used by a class to use an interface defined previously?
+a) import
+b) Import
+c) implements
+d) Implements
+View Answer
+
+Answer: c
+Explanation: interface is inherited by a class using implements.*/
