20bca65ebd33b0602be4781e7454ec12b97f1427
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:10:27 +0530

    Create 3.java

---
+++ Collection MCQ/3.java
@@ -0,0 +1,9 @@
+/*Which of this interface is not a part of Java’s collection framework?
+a) List
+b) Set
+c) SortedMap
+d) SortedList
+View Answer
+
+Answer: d
+Explanation: SortedList is not a part of collection framework.*/
