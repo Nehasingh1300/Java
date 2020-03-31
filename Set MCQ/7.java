a57c8ff3fa8905ed97855e494692261802127158
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:51:17 +0530

    Create 7.java

---
+++ Set MCQ/7.java
@@ -0,0 +1,7 @@
+/*Set has contains(Object o) method.
+a) True
+b) False
+View Answer
+
+Answer: a
+Explanation: Set has contains(Object o) method instead of get(Object o) method as get is needed for comparing object and getting corresponding value.*/
