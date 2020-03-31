7ba8f6f514e7cfbb728ef467c1c1653175896654
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:52:12 +0530

    Create 10.java

---
+++ Set MCQ/10.java
@@ -0,0 +1,9 @@
+/*What is the unique feature of LinkedHashSet?
+a) It is not a valid class
+b) It maintains the insertion order and guarantees uniqueness
+c) It provides a way to store key values with uniqueness
+d) The elements in the collection are linked to each other
+View Answer
+
+Answer: b
+Explanation: Set is a collection of unique elements.HashSet has the behavior of Set and stores key value pairs. The LinkedHashSet stores the key value pairs in the order of insertion.*/
