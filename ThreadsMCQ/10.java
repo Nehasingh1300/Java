09d2e9caebdf935ade91df683db8932d38bdc5a5
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:34:38 +0530

    Create 10.java

---
+++ ThreadsMCQ/10.java
@@ -0,0 +1,9 @@
+/*Thread priority in Java is?
+a) Integer
+b) Float
+c) double
+d) long
+View Answer
+
+Answer: a
+Explanation: Java assigns to each thread a priority that determines hoe that thread should be treated with respect to others. Thread priority is integers that specify relative priority of one thread to another.*/
