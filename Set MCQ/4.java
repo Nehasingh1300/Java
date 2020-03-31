285ae4ab3328fa31402597e0a82f730e8f7055c2
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:50:08 +0530

    Create 4.java

---
+++ Set MCQ/4.java
@@ -0,0 +1,9 @@
+/*What are the initial capacity and load factor of HashSet?
+a) 10, 1.0
+b) 32, 0.75
+c) 16, 0.75
+d) 32, 1.0
+View Answer
+
+Answer: c
+Explanation: We should not set the initial capacity too high and load factor too low if iteration performance is needed.*/
