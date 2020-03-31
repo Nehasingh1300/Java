d4148b382724c913d1706b02090d1f0efb0a2de7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:19:59 +0530

    Create 2.java

---
+++ Interface MCQ/2.java
@@ -0,0 +1,9 @@
+/*Which of these can be used to fully abstract a class from its implementation?
+a) Objects
+b) Packages
+c) Interfaces
+d) None of the Mentioned
+View Answer
+
+Answer: c
+Explanation: None.*/
