0b098b8e7e0d3426e0f0941a7e40b572669abe27
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:49:30 +0530

    Create 2.java

---
+++ Set MCQ/2.java
@@ -0,0 +1,7 @@
+/*Do we have get(Object o) method in HashSet.
+a) True
+b) False
+View Answer
+
+Answer: b
+Explanation: get(Object o) method is useful when we want to compare objects based on the comparison of values. HashSet does not provide any way to compare objects. It just guarantees unique objects stored in the collection.*/
