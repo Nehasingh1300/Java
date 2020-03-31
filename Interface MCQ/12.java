f4d48ad7aeb5a7205cdd60fe07be775dbf060718
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:34:40 +0530

    Create 12.java

---
+++ Interface MCQ/12.java
@@ -0,0 +1,9 @@
+/*What type of methods an interface contain by default?
+a) abstract
+b) static
+c) final
+d) private
+View Answer
+
+Answer: a
+Explanation: By default, interface contains abstract methods. The abstract methods need to be implemented by concrete classes.*/
