d122e612652508ea34462dbfaccb45b114bf822b
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:43:59 +0530

    Create 19.java

---
+++ ThreadsMCQ/19.java
@@ -0,0 +1,7 @@
+/*Deadlock is a situation when thread is waiting for other thread to release acquired object.
+a) True
+b) False
+View Answer
+
+Answer: a
+Explanation: Deadlock is java programming situation where one thread waits for an object lock that is acquired by other thread and vice-versa.*/
