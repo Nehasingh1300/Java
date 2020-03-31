7b309cdae7babfb757f29bc8565ff66c566993c9
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:41:52 +0530

    Create 16.java

---
+++ ThreadsMCQ/16.java
@@ -0,0 +1,9 @@
+/*What does not prevent JVM from terminating?
+a) Process
+b) Daemon Thread
+c) User Thread
+d) JVM Thread
+View Answer
+
+Answer: b
+Explanation: Daemon thread runs in the background and does not prevent JVM from terminating. Child of daemon thread is also daemon thread.*/
