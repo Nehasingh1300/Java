67ae133c118506326bd8f17df5838e96cb73ee54
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:02:08 +0530

    Create 31.java

---
+++ ThreadsMCQ/31.java
@@ -0,0 +1,9 @@
+/*What is synchronization in reference to a thread?
+a) It’s a process of handling situations when two or more threads need access to a shared resource
+b) It’s a process by which many thread are able to access same shared resource simultaneously
+c) It’s a process by which a method is able to access many different threads simultaneously
+d) It’s a method that allow too many threads to access any information the require
+View Answer
+
+Answer: a
+Explanation: When two or more threads need to access the same shared resource, they need some way to ensure that the resource will be used by only one thread at a time, the process by which this is achieved is called synchronization*/
