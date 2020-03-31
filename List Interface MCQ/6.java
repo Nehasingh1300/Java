b340ea4f97a71309e6069ce5bc33aac65dd85fbb
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:45:31 +0530

    Create 6.java

---
+++ List Interface MCQ/6.java
@@ -0,0 +1,9 @@
+/*What is the difference between length() and size() of ArrayList?
+a) length() and size() return the same value
+b) length() is not defined in ArrayList
+c) size() is not defined in ArrayList
+d) length() returns the capacity of ArrayList and size() returns the actual number of elements stored in the list
+View Answer
+
+Answer: d
+Explanation: length() returns the capacity of ArrayList and size() returns the actual number of elements stored in the list which is always less than or equal to capacity.*/
