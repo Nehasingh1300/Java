6cd232cfeaf5f92e6a5500ddecc022ad5522cd1a
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:33:50 +0530

    Create 9.java

---
+++ ThreadsMCQ/9.java
@@ -0,0 +1,9 @@
+/* Which of these are types of multitasking?
+a) Process based
+b) Thread based
+c) Process and Thread based
+d) None of the mentioned
+View Answer
+
+Answer: c
+Explanation: There are two types of multitasking: Process based multitasking and Thread based multitasking.*/
