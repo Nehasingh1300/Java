87a612e56467c59f83c624d780a40a17a1ae4ac2
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:43:23 +0530

    Create 18.java

---
+++ ThreadsMCQ/18.java
@@ -0,0 +1,9 @@
+/*What is true about time slicing?
+a) Time slicing is OS service that allocates CPU time to available runnable thread
+b) Time slicing is the process to divide the available CPU time to available runnable thread
+c) Time slicing depends on its implementation in OS
+d) Time slicing allocates more resources to thread
+View Answer
+
+Answer: b
+Explanation: Time slicing is the process to divide the available CPU time to available runnable thread.*/
