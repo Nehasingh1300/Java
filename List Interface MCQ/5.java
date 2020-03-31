16fabd1114d8db37c2aebb787390ed11086939ac
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:44:58 +0530

    Create 5.java

---
+++ List Interface MCQ/5.java
@@ -0,0 +1,9 @@
+/*How is Arrays.asList() different than the standard way of initialising List?
+a) Both are same
+b) Arrays.asList() throws compilation error
+c) Arrays.asList() returns a fixed length list and doesn’t allow to add or remove elements
+d) We cannot access the list returned using Arrays.asList()
+View Answer
+
+Answer: c
+Explanation: List returned by Arrays.asList() is a fixed length list which doesn’t allow us to add or remove element from it.add() and remove() method will throw UnSupportedOperationException if used.*/
