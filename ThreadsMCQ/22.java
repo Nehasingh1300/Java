d9788bb327629a9a48dd6a452b61795581df0ac7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:45:42 +0530

    Create 22.java

---
+++ ThreadsMCQ/22.java
@@ -0,0 +1,9 @@
+/*Which of the following is a correct constructor for thread?
+a) Thread(Runnable a, String str)
+b) Thread(int priority)
+c) Thread(Runnable a, int priority)
+d) Thread(Runnable a, ThreadGroup t)
+View Answer
+
+Answer: a
+Explanation: Thread(Runnable a, String str) is a valid constructor for thread. Thread() is also a valid constructor.*/
