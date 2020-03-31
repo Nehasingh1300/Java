e6f37c7ce2e197710988044338b0bd68fb01ae44
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:22:43 +0530

    Create 1.java

---
+++ ThreadsMCQ/1.java
@@ -0,0 +1,9 @@
+/*1. Which of these method of Thread class is used to find out the priority given to a thread?
+a) get()
+b) ThreadPriority()
+c) getPriority()
+d) getThreadPriority()
+View Answer
+
+Answer: c
+Explanation: None.*/
