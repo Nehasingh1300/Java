715e55e31852a2580a7fcd11297fb5e0145ea536
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:45:23 +0530

    Create 21.java

---
+++ ThreadsMCQ/21.java
@@ -0,0 +1,9 @@
+/*What is true about threading?
+a) run() method calls start() method and runs the code
+b) run() method creates new thread
+c) run() method can be called directly without start() method being called
+d) start() method creates new thread and calls code written in run() method
+View Answer
+
+Answer: d
+Explanation: start() eventually calls run() method. Start() method creates thread and calls the code written inside run method.*/
