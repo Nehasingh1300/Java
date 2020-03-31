01b73bf29fecc7e7ca469565d17684b1523fb445
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:36:48 +0530

    Create 13.java

---
+++ Interface MCQ/13.java
@@ -0,0 +1,9 @@
+/*What will happen if we provide concrete implementation of method in interface?
+a) The concrete class implementing that method need not provide implementation of that method
+b) Runtime exception is thrown
+c) Compilation failure
+d) Method not found exception is thrown
+View Answer
+
+Answer: c
+Explanation: The methods of interfaces are always abstract. They provide only method definition.*/
