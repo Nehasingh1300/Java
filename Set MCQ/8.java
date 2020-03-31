45925afa71fa94b323c1d95057852b7be5a34001
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:51:34 +0530

    Create 8.java

---
+++ Set MCQ/8.java
@@ -0,0 +1,9 @@
+/* What is the difference between TreeSet and SortedSet?
+a) TreeSet is more efficient than SortedSet
+b) SortedSet is more efficient than TreeSet
+c) TreeSet is an interface; SortedSet is a concrete class
+d) SortedSet is an interface; TreeSet is a concrete class
+View Answer
+
+Answer: d
+Explanation: SortedSet is an interface. It maintains an ordered set of elements. TreeSet is an implementation of SortedSet.*/
