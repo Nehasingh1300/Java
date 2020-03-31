8e39b75db20d2b1ccdf3b187083bffab546aafd5
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:47:03 +0530

    Create 9.java

---
+++ List Interface MCQ/9.java
@@ -0,0 +1,9 @@
+/* What is the worst case complexity of accessing an element in ArrayList?
+a) O(n)
+b) O(1)
+c) O(nlogn)
+d) O(2)
+View Answer
+
+Answer: b
+Explanation: ArrayList has O(1) complexity for accessing an element in ArrayList. O(n) is the complexity for accessing an element from LinkedList.*/
