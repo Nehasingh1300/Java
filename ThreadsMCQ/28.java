2dfbd0b9aa65947382a2860f7793c17be818b803
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:59:21 +0530

    Create 28.java

---
+++ ThreadsMCQ/28.java
@@ -0,0 +1,9 @@
+/*Which of these method is used to tell the calling thread to give up a monitor and go to sleep until some other thread enters the same monitor?
+a) wait()
+b) notify()
+c) notifyAll()
+d) sleep()
+View Answer
+
+Answer: a
+Explanation: wait() method is used to tell the calling thread to give up a monitor and go to sleep until some other thread enters the same monitor. This helps in avoiding polling and minimizes CPU idle time.*/
