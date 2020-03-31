a158d496ae9b7956f60b24d0f7117f911b6d3893
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:23:31 +0530

    Create 2.java

---
+++ ThreadsMCQ/2.java
@@ -0,0 +1,9 @@
+/*Which of these method of Thread class is used to Suspend a thread for a period of time?
+a) sleep()
+b) terminate()
+c) suspend()
+d) stop()
+View Answer
+
+Answer: a
+Explanation: None.*/
