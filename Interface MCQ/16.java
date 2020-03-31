e05c4154c4d1eef692573963b2af1d6c902a86e7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:38:48 +0530

    Create 16.java

---
+++ Interface MCQ/16.java
@@ -0,0 +1,7 @@
+/*Can “abstract” keyword be used with constructor, Initialization Block, Instance Initialization and Static Initialization Block.
+a) True
+b) False
+View Answer
+
+Answer: b
+Explanation: No, Constructor, Static Initialization Block, Instance Initialization Block and variables cannot be abstract.*/
