81b2140c79440a075f3190bf834193021faec4e7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:37:59 +0530

    Create 13.java

---
+++ ThreadsMCQ/13.java
@@ -0,0 +1,22 @@
+//What will be the output of the following Java code?
+
+    class multithreaded_programing
+    {
+        public static void main(String args[])
+        {
+            Thread t = Thread.currentThread();
+            System.out.println(t);        
+        }
+    }
+/*a) Thread[5,main]
+b) Thread[main,5]
+c) Thread[main,0]
+d) Thread[main,5,main]
+View Answer
+
+Answer: d
+Explanation: None.
+Output:
+$ javac multithreaded_programing.java
+$ java multithreaded_programing
+Thread[main,5,main]*/
