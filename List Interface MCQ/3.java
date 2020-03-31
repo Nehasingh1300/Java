92738aff079229cb75035d74dc2546b5b4c041f1
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:42:31 +0530

    Create 3.java

---
+++ List Interface MCQ/3.java
@@ -0,0 +1,9 @@
+/*How to sort elements of ArrayList?
+a) Collection.sort(listObj);
+b) Collections.sort(listObj);
+c) listObj.sort();
+d) Sorter.sortAsc(listObj);
+View Answer
+
+Answer: b
+Explanation: Collections provides a method to sort the list. The order of sorting can be defined using Comparator.*/
