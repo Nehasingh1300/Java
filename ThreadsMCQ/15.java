a09697f0c85885372a35060f2ebd062c2d0af4ea
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:40:17 +0530

    Create 15.java

---
+++ ThreadsMCQ/15.java
@@ -0,0 +1,11 @@
+/*This set of Java Quiz focuses on “Multithreading”.
+
+1. What requires less resources?
+a) Thread
+b) Process
+c) Thread and Process
+d) Neither Thread nor Process
+View Answer
+
+Answer: a
+Explanation: Thread is a lightweight and requires less resources to create and exist in the process. Thread shares the process resources.*/
