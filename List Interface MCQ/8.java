e00b40b1a28498b77d40035dbe7033b9f9d2320f
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:46:34 +0530

    Create 8.java

---
+++ List Interface MCQ/8.java
@@ -0,0 +1,9 @@
+/*Which of the below is not an implementation of List interface?
+a) RoleUnresolvedList
+b) Stack
+c) AttibuteList
+d) SessionList
+View Answer
+
+Answer: d
+Explanation: SessionList is not an implementation of List interface. The others are concrete classes of List.*/
