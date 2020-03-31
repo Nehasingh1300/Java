530f452102994090c9c5914ee606db49738b8a8c
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:41:54 +0530

    Create 2.java

---
+++ List Interface MCQ/2.java
@@ -0,0 +1,9 @@
+/*How to remove duplicates from List?
+a) HashSet<String> listToSet = new HashSet<String>(duplicateList);
+b) HashSet<String> listToSet = duplicateList.toSet();
+c) HashSet<String> listToSet = Collections.convertToSet(duplicateList);
+d) HashSet<String> listToSet = duplicateList.getSet();
+View Answer
+
+Answer: a
+Explanation: Duplicate elements are allowed in List. Set contains unique objects.*/
