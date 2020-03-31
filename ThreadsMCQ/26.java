b92eb27962e57a6dab4e3612aee307d59ae08bfd
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:56:12 +0530

    Create 26.java

---
+++ ThreadsMCQ/26.java
@@ -0,0 +1,9 @@
+/*Which of this method is used to avoid polling in Java?
+a) wait()
+b) notify()
+c) notifyAll()
+d) all of the mentioned
+View Answer
+
+Answer: d
+Explanation: Polling is a usually implemented by looping in CPU is wastes CPU time, one thread being executed depends on other thread output and the other thread depends on the response on the data given to the first thread. In such situation CPU time is wasted, in Java this is avoided by using methods wait(), notify() and notifyAll().*/
