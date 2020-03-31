86b087f4cbd25425a2639078dff58be0c3a3672e
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:34:16 +0530

    Create 11.java

---
+++ Interface MCQ/11.java
@@ -0,0 +1,9 @@
+/*What does an interface contain?
+a) Method definition
+b) Method declaration
+c) Method declaration and definition
+d) Method name
+View Answer
+
+Answer: b
+Explanation: Interface contains the only declaration of the method.*/
