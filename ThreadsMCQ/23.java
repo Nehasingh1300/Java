9ee68463baef736117f5d942d03213d3c9d198ca
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 21:46:03 +0530

    Create 23.java

---
+++ ThreadsMCQ/23.java
@@ -0,0 +1,9 @@
+/*Which of the following stops execution of a thread?
+a) Calling SetPriority() method on a Thread object
+b) Calling notify() method on an object
+c) Calling wait() method on an object
+d) Calling read() method on an InputStream object
+View Answer
+
+Answer: b
+Explanation: notify() wakes up a single thread which is waiting for this object.*/
