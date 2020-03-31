bbc0039d1c5f469b86f94638a8ec6c568fd60317
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:49:52 +0530

    Create 3.java

---
+++ Set MCQ/3.java
@@ -0,0 +1,9 @@
+/*What does Collections.emptySet() return?
+a) Immutable Set
+b) Mutable Set
+c) The type of Set depends on the parameter passed to the emptySet() method
+d) Null object
+View Answer
+
+Answer: a
+Explanation: Immutable Set is useful in multithreaded environment. One does not need to declare generic type collection. It is inferred by the context of method call.*/
