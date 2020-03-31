b431b5970f611e4ab492d90629e0e245019872b5
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:25:28 +0530

    Create 6.java

---
+++ Interface MCQ/6.java
@@ -0,0 +1,9 @@
+/*Which of the following is an incorrect statement about packages?
+a) Interfaces specifies what class must do but not how it does
+b) Interfaces are specified public if they are to be accessed by any code in the program
+c) All variables in interface are implicitly final and static
+d) All variables are static and methods are public if interface is defined pubic
+View Answer
+
+Answer: d
+Explanation: All methods and variables are implicitly public if interface is declared public.*/
