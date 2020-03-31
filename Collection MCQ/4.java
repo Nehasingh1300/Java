6de18b8a2d2d81ac6e6c00d1ceed1683c9262af7
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:10:54 +0530

    Create 4.java

---
+++ Collection MCQ/4.java
@@ -0,0 +1,9 @@
+/*Which of these methods deletes all the elements from invoking collection?
+a) clear()
+b) reset()
+c) delete()
+d) refresh()
+View Answer
+
+Answer: a
+Explanation: clear() method removes all the elements from invoking collection.*/
