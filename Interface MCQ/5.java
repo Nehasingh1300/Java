ca59c8f29f501502bfd9274f255c9505edca7951
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-03-31 22:24:55 +0530

    Create 5.java

---
+++ Interface MCQ/5.java
@@ -0,0 +1,8 @@
+/*Which of the following is the correct way of implementing an interface salary by class manager?
+a) class manager extends salary {}
+b) class manager implements salary {}
+c) class manager imports salary {}
+d) none of the mentioned
+View Answer
+
+Answer: b*/
