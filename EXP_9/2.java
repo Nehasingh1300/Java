0065d844de66c4064cd060a58dee06ed203a3dec
Neha Singh <40912375+Nehasingh1300@users.noreply.github.com>
2020-04-03 22:35:30 +0530

    Create 2.java

---
+++ EXP_9/2.java
@@ -0,0 +1,52 @@
+package thread;
+
+
+	class even extends Thread
+	{
+	Thread t=null;
+	even()
+	{
+	t=new Thread(this);
+	start();
+	}
+	public void run()
+	{
+	try
+	{
+	for(int i=2;i<50;i+=2)
+	System.out.print(i+" ");
+	Thread.sleep(100);
+	}
+	catch(Exception e)
+	{System.out.println("thread interepted");}
+	}
+	}
+	class odd extends Thread
+	{
+	Thread t=null;
+	odd()
+	{
+	t=new Thread(this);
+	start();
+	}
+	public void run()
+	{
+	try
+	{
+	for(int i=1;i<50;i+=2)
+	System.out.print(i+" ");
+	Thread.sleep(100);
+	}
+	catch(Exception e)
+	{System.out.println("thread interepted");}
+	}
+	}
+	class EvenOdd
+	{
+	public static void main(String arg[])
+	{
+	
+	even e=new even();
+	odd o=new odd();
+	}
+	}
